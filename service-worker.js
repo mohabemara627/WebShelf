d9873262aa76bdd616800036b953134bfef0d3b0
Nameless <mohabemaara@gmail.com>
2026-09-05 18:42:04 +0300

    WebShelf

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const SCOPE_PATH = new URL(self.registration.scope).pathname;
 const CACHE_PREFIX = 'webshelf-' + SCOPE_PATH + '-';
-const CACHE_NAME = CACHE_PREFIX + '68eb3c951599';
+const CACHE_NAME = CACHE_PREFIX + 'cb273d3df3ca';
 const CORE = [
   "./",
   "./style.css",
