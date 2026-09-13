96218752d8b45f2b5c23dc6576ef716a89fe31d3
Nameless <mohabemaara@gmail.com>
2026-09-13 23:03:59 +0300

    upd

--- data.js
+++ data.js
@@ -13,7 +13,8 @@ const WebShelfCategories = [
         "url": "https://anime.nexus/",
         "icon": "./images/icons/animenexus.png",
         "badges": [
-          "Ar"
+          "Ar",
+          "4K"
         ]
       },
       {
@@ -83,17 +84,18 @@ const WebShelfCategories = [
     "accent": "var(--TV-streaming)",
     "sites": [
       {
-        "name": "Movy",
-        "url": "https://www.movy.bz/",
-        "icon": "./images/icons/movy.png",
-        "badges": [
-          "Ar"
-        ]
-      },
-      {
         "name": "Cinejoy",
         "url": "https://cinejoy.to/",
         "icon": "./images/icons/cinejoy.ico",
+        "badges": [
+          "Ar",
+          "4K"
+        ]
+      },
+      {
+        "name": "Movy",
+        "url": "https://www.movy.bz/",
+        "icon": "./images/icons/movy.png",
         "badges": [
           "Ar"
         ]
